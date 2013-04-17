f6f5014f030e9c589fbb2a548eb1c701ad9c68c8
Inoue_Kenta <NQuang17@Gmail.com>
2013-04-17 13:27:13 +0700

    lap trinh file CSV.cpp

--- DoAnGK/CSV.cpp
+++ DoAnGK/CSV.cpp
@@ -3,6 +3,22 @@
 void initCSV(RAW_CSV &csv, char *fpath)
 {
 	FILE *f = fopen(fpath, "rt");
-
-	fclose;
+	int count_EOLINE=0;
+	if(f != NULL)
+	{
+		while(fgetc(f) != EOF)
+		{
+			if(fgetc(f) == '\n')
+				count_EOLINE++;
+		}
+	}											/* Dem so dong trong file */
+	csv= new char*[count_EOLINE + 1];			/* Cap phat dong cho mang csv */
+	int i=0;
+	while(!feof(f))
+		{
+			fgets(csv[i],10000,f);
+			i++;
+		}
+		csv[i]=NULL;
+	fclose(f);
 }

0a28654b1693ef61f7d3598da999932dbc82774e
Inoue_Kenta <NQuang17@Gmail.com>
2013-04-17 07:55:48 +0700

    bo sung tham so ham main

--- DoAnGK/main.cpp
+++ DoAnGK/main.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void main()
+void main(int argc,char **argv)
 {
 	//test
+	if(argc<3)
+		printf("Usage:generate-html.exe FilePath<CSV> Path-<result>");
+	/* thong so ham main su dung: 
+	1.ten chuong trinh
+	2.duong dan toi file CSV
+	3.duong dan toi thu muc ket qua
+	*/
+	else
+	{
 	char s[1024];
-	FILE *f = fopen("data.csv", "rt");
+	FILE *f = fopen(argv[1], "rt"); //Lay file CSV
 	while (!feof(f)) {
 	fgets(s, 1024, f);
 
@@ -16,5 +25,5 @@ void main()
 	}
 	}
 	fclose(f);
-	
+	}
 }

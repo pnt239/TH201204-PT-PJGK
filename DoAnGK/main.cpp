ba0077ea6ba59e073dcd62add392017b280fec2b
TJoan Pano <pano.tjoan@yahoo.com>
2013-04-18 20:17:57 +0700

    Test ham explode trong ham main

--- DoAnGK/main.cpp
+++ DoAnGK/main.cpp
@@ -1,7 +1,17 @@
 #include "CSV.h"
+#include "stringex.h"
 
-void main(int argc,char **argv)
+int main(int argc,char **argv)
 {
-	RAW_CSV raw_data;
-	printf("%d\n", initCSV(raw_data, "data.csv"));
+	//RAW_CSV raw_data;
+	//printf("%d\n", initCSV(raw_data, "data.csv"));
+	char str[] ="Pham, Ngoc Thanh";
+	char **s = explode(str, ", ");
+	printf("%s'\n%s'\n", s[0], s[1]);
+
+	delete s[0];
+	delete s[1];
+	delete s;
+
+	return 0;
 }

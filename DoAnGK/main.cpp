5c416f1d447e79cfc19bc76d173d56df4b7cb01c
Pham Ngoc Thanh <panota.joan@gmail.com>
2013-04-18 18:24:05 +0700

    Update main
    chạy thử hàm initCSV

--- DoAnGK/main.cpp
+++ DoAnGK/main.cpp
@@ -1,29 +1,7 @@
-#include <stdio.h>
-#include <string.h>
+#include "CSV.h"
 
 void main(int argc,char **argv)
 {
-	//test
-	if(argc<3)
-		printf("Usage:generate-html.exe FilePath<CSV> Path-<result>");
-	/* thong so ham main su dung: 
-	1.ten chuong trinh
-	2.duong dan toi file CSV
-	3.duong dan toi thu muc ket qua
-	*/
-	else
-	{
-	char s[1024];
-	FILE *f = fopen(argv[1], "rt"); //Lay file CSV
-	while (!feof(f)) {
-	fgets(s, 1024, f);
-
-	char *word = strtok(s, ",");
-	while (word != NULL) {
-		printf("%s\n", word);
-		word = strtok(NULL, ",");
-	}
-	}
-	fclose(f);
-	}
+	RAW_CSV raw_data;
+	printf("%d\n", initCSV(raw_data, "data.csv"));
 }

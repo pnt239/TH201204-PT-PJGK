6716860d34f64a108f9e788f8892a68f67270e89
Inoue_Kenta <NQuang17@Gmail.com>
2013-04-18 08:25:52 +0700

    viet ham strsplit

--- DoAnGK/stringex.cpp
+++ DoAnGK/stringex.cpp
@@ -3,6 +3,28 @@
 char** strsplit(char *str, char *delim)
 {
 	char **ret;
+	int n=strlen(delim);
+	int i=0;
+	int count_delim=0;
+	while(str[i] != NULL)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(str[i]==delim[j])
+				count_delim++;
+		}
+		i++;
+	}		//Dem so luong phan tu con cua delim trong str
+	ret= new char*[count_delim+1];
+	char *pch= strtok(str,delim);
+	i=0;
+	while(pch != NULL)
+	{
+		ret[i]=pch;
+		i++;
+		pch=strtok(NULL,delim);
+	}
+	ret[i]=NULL;
 	return ret;
 }
 

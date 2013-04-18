a456b6354e86ba90dfd363a461ce3eeee34abbfc
Pham Ngoc Thanh <panota.joan@gmail.com>
2013-04-18 18:23:38 +0700

    Update initCSV
    Sửa lại hàm đếm line, cấp phát csv cho tiết kiệm hơn

--- DoAnGK/CSV.cpp
+++ DoAnGK/CSV.cpp
@@ -2,23 +2,37 @@
 
 int initCSV(RAW_CSV &csv, char *fpath)
 {
-	int LINE_COUNT =0;
+	/* max_numchar : so ky tu ma dong dai nhat co */
+	int line_count = 1, i = 0, cur_numchar = 0, max_numchar = 0;
+	char ctemp;
 
 	FILE *f = fopen(fpath, "rt");
-	if(f != NULL) return -1;
+	if(f == NULL) return -1;
 
-	while(fgetc(f) != EOF)
-	{
-		if(fgetc(f) == '\n')
-			LINE_COUNT++;
-	}											/* Dem so dong trong file */
-	csv= new char*[LINE_COUNT + 1];			/* Cap phat dong cho mang csv */
-	int i=0;
-	while(!feof(f))
-		{
-			fgets(csv[i],10000,f);
-			i++;
+	/* Dem so dong trong file */
+	while(!feof(f)) {
+		ctemp = fgetc(f);
+		if(ctemp == '\n') {
+			line_count++;
+			if (cur_numchar > max_numchar) max_numchar = cur_numchar;
+			cur_numchar = 0;
+		} else {
+			cur_numchar++;
 		}
-		csv[i]=NULL;
+	}
+	if (cur_numchar > max_numchar) max_numchar = cur_numchar;
+	/* Tra ve vi tri dau file */
+	fseek(f, 0, SEEK_SET);
+	/* Cap phat dong cho mang csv */								
+	csv= new char*[line_count + 1];			
+	
+	while(!feof(f))
+	{
+		csv[i] = new char[max_numchar];
+		fgets(csv[i], max_numchar, f);
+		i++;
+	}
+
+	csv[i]=NULL;
 	fclose(f);
 }
